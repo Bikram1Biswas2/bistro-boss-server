0b606b1e16d0b10fc4a3f219b911ad4386bdb560
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-02 12:40:20 +0530

    create a delete route for delete a specific cart items

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 
@@ -52,11 +52,18 @@ async function run() {
         const result = await cartCollection.find(query).toArray()
         res.send(result)
     })
-
     //   post a cart
     app.post('/carts',async(req,res)=>{
         const cartItems = req.body 
         const result = await cartCollection.insertOne(cartItems)
+        res.send(result)
+    })
+
+    // delete a cart
+    app.delete('/carts/:id',async(req,res)=>{
+        const id = req.params.id 
+        const query = {_id: new ObjectId(id)}
+        const result = await cartCollection.deleteOne(query)
         res.send(result)
     })
 
