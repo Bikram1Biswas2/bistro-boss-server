06cc88bed993d03907b221a7c20bf9f42c4deb18
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-04 21:43:37 +0530

    validate if one users data is saved in database this users data is not saved anymore after he/she loggedin

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ async function run() {
     // user related apis
     app.post('/users',async(req,res)=>{
         const user = req.body
+        const query= {email:user.email}
+        const existingUser = await userCollection.findOne(query)
+        if(existingUser){
+            return res.send({message:'user already exist',insertedId:null})
+        }
         const result = await userCollection.insertOne(user)
         res.send(result)
     })
