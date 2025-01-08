b05702f00b4d60fd284d10aed3bfd135f1736aff
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-08 16:15:37 +0530

    implement delete functionality successfully

--- index.js
+++ index.js
@@ -138,6 +138,13 @@ async function run() {
         res.send(result)
     })
 
+    app.delete('/menu/:id',verifyToken,verifyAdmin,async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: id}
+        const result = await menuCollection.deleteOne(query)
+        res.send(result)
+    })
+
     // Review-Related Endpoints
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
